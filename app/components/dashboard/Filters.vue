e53a37e411c6780b7c10408193c08eb2396ec556
THEMING <306081148@qq.com>
2025-07-15 22:36:58 +0800

    Update Filters.vue

--- app/components/dashboard/Filters.vue
+++ app/components/dashboard/Filters.vue
@@ -76,7 +76,7 @@ onBeforeMount(() => {
                   selectedLinks.includes(link.slug) ? 'opacity-100' : 'opacity-0',
                 )"
               />
-              {{ link.slug }}
+              {{ link.comment }}
             </CommandItem>
           </VList>
         </CommandGroup>
